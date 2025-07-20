845e38870720a1ae4fcf6e5d3b8eb3c37adda375
RB245 <rachnabajoria04@gmail.com>
2025-07-20 17:19:22 +0530

    debug2

--- configs/mongodbConnection.js
+++ configs/mongodbConnection.js
@@ -1,33 +1,15 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async()=>{
-//     try{
-//         await mongoose.connect(process.env.MONGO_URI
-//             // useNewUrlParser:true,
-//             // useUnifiedTopology:true,
-//             );
-//         console.log("MongoDB connected");
-//     }catch(error){
-//         console.log(`Error connecting to MongoDB: ${error}`);
-//     }
-// }
-
-// module.exports=connectDB;
-
-
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection error:", error.message);
-        process.exit(1);
+const connectDB = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI
+            // useNewUrlParser:true,
+            // useUnifiedTopology:true,
+            );
+        console.log("MongoDB connected");
+    }catch(error){
+        console.log(`Error connecting to MongoDB: ${error}`);
     }
-};
+}
 
-module.exports = connectDB;
+module.exports=connectDB;
